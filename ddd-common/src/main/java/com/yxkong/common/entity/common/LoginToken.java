b479a1a658d1ca90481efbcf57f81354ae816026
yxkong <5ycode@sina.com>
2021-07-22 09:41:59 +0800

    去除一些信息

--- ddd-common/src/main/java/com/yxkong/common/entity/common/LoginToken.java
+++ ddd-common/src/main/java/com/yxkong/common/entity/common/LoginToken.java
@@ -49,32 +49,18 @@ public class LoginToken implements Serializable {
      */
     private String birthday;
     /**
-     * 没有赋值
+     * 登录类型
      */
     private String loginType;
     /**
-     * 集团用户中心返回的uuid
-     */
-    private String uuid;
-    /**
-     * 没有赋值
+     * 登录时间
      */
     private String loginTime;
     /**
      * 加密的用户来源，说明decodeSource
      */
     private String source;
-    /**
-     * 用户来源0：万卡，1：叮当，2：闪白条，3：品质商场，" +
-     * "4：钱包，5：超能二类，6：超能5类，7：超能地推，" +
-     * "8：向阳花，9：ofo，10：白条，1000以上：三体定制化来源
-     */
-    private Integer decodeSource;
-    /**
-     * 集团用户中心客户号
-     */
-    private String custNo;
-    private Integer isBack;
+
     /**
      * imei
      */
@@ -83,22 +69,18 @@ public class LoginToken implements Serializable {
      * 用户标签
      */
     private String lable;
+
     /**
-     * 路由化标识0:老用户，1 路由化用户 2 额度三体化用户
-     */
-    private Integer isRoute;
-    /**
-     * 没有赋值
+     * 登录渠道
      */
     private String loginProId;
     /**
      * 当前操作渠道，在loginProId没值时使用
      */
     private String proId;
-    private String createTime;
     private String exprieTime;
     /**
-     * 租户id：万卡 1001 小房卡 2001 亿贝卡 2005
+     * 租户id：
      */
     private Integer tenantId;
 
@@ -198,13 +180,7 @@ public class LoginToken implements Serializable {
         this.loginType = loginType;
     }
 
-    public String getUuid() {
-        return uuid;
-    }
 
-    public void setUuid(String uuid) {
-        this.uuid = uuid;
-    }
 
     public String getLoginTime() {
         return loginTime;
@@ -222,29 +198,7 @@ public class LoginToken implements Serializable {
         this.source = source;
     }
 
-    public Integer getDecodeSource() {
-        return decodeSource;
-    }
 
-    public void setDecodeSource(Integer decodeSource) {
-        this.decodeSource = decodeSource;
-    }
-
-    public String getCustNo() {
-        return custNo;
-    }
-
-    public void setCustNo(String custNo) {
-        this.custNo = custNo;
-    }
-
-    public Integer getIsBack() {
-        return isBack;
-    }
-
-    public void setIsBack(Integer isBack) {
-        this.isBack = isBack;
-    }
 
     public String getImei() {
         return imei;
@@ -262,13 +216,6 @@ public class LoginToken implements Serializable {
         this.lable = lable;
     }
 
-    public Integer getIsRoute() {
-        return isRoute;
-    }
-
-    public void setIsRoute(Integer isRoute) {
-        this.isRoute = isRoute;
-    }
 
     public String getLoginProId() {
         return loginProId;
@@ -278,13 +225,6 @@ public class LoginToken implements Serializable {
         this.loginProId = loginProId;
     }
 
-    public String getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(String createTime) {
-        this.createTime = createTime;
-    }
 
     public String getExprieTime() {
         return exprieTime;
