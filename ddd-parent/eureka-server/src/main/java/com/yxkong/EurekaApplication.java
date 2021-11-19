fa9c0fca179ac9cbac851978b70ad39551ae337a
yxkong <5ycode@sina.com>
2021-11-19 20:45:48 +0800

    eureka-server

---
+++ ddd-parent/eureka-server/src/main/java/com/yxkong/EurekaApplication.java
@@ -0,0 +1,20 @@
+package com.yxkong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * <TODO>
+ *
+ * @Author: yxkong
+ * @Date: 2021/11/19 8:15 PM
+ * @version: 1.0
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+    }
+}
