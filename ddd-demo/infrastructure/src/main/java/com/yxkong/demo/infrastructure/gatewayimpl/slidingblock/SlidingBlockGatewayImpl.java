e6a3064f04fe0ff10fc799d58f4fcd9ad9dacd55
yxkong <5ycode@sina.com>
2021-11-23 20:07:27 +0800

    update

---
+++ ddd-demo/infrastructure/src/main/java/com/yxkong/demo/infrastructure/gatewayimpl/slidingblock/SlidingBlockGatewayImpl.java
@@ -0,0 +1,7 @@
+package com.yxkong.demo.infrastructure.gatewayimpl.slidingblock;/**
+ * <TODO>
+ * @Author: yxkong
+ * @Date: 2021/11/23 10:54 AM
+ * @version: 1.0
+ */public class SlidingBlockGatewayImpl {
+}
