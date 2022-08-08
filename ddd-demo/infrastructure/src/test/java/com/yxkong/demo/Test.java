895a7ebb64393612f796670efbc56444dabadc86
yxkong <5ycode@sina.com>
2022-08-08 13:56:41 +0800

    update

---
+++ ddd-demo/infrastructure/src/test/java/com/yxkong/demo/Test.java
@@ -0,0 +1,15 @@
+package com.yxkong.demo;
+
+/**
+ * <TODO>
+ *
+ * @Author: yxkong
+ * @Date: 2021/11/30 9:11 AM
+ * @version: 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2-1);
+
+    }
+}
